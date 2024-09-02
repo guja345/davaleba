35179bc244fb518ad2926f191200e39a9cedaf85
guja345 <elgujarobaqa@gmail.com>
2024-09-02 21:22:17 +0400

    add just first two lines

--- src/TicketService.java
+++ src/TicketService.java
@@ -2,6 +2,8 @@
 
 public class TicketService {
 
+    Ticket [] storageofTickets = new Ticket[]{new Ticket("123", "main", "256", 124567, true, "a", 13.0, 50.00)};
+
     public static void main(String[] args) {
         var emptyTicket = new Ticket();
         var fullTicket = new Ticket("Id12", "Main hall", "256",1693430400L, true,'A', 25.00, 50.0);
